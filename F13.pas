916e04b3a5ae7de7a4abcc3ecfe4f28930f4efe4
Rainburn <49584704+Rainburn@users.noreply.github.com>
2019-04-22 01:05:48 +0700

    Update F13.pas

--- F13.pas
+++ F13.pas
@@ -62,6 +62,22 @@ interface
 	arrDataBuku : array[1..NMax] of DataBuku;
 	// End of Variable to Read Buku.csv
 
+
+	// Variables to Read Pengembalian.csv
+	filepengembalian : textfile;
+	fkembali : file of char;
+	lenKembali : longint;
+	tipeDataKembali : integer;
+	arrDataKembali : array [1..NMax] of DataKembali;
+
+	// Variable to Read Kembali.csv
+	// Variables to Read Pengembalian.csv
+	filehilang : textfile;
+	fHilang : file of char;
+	lenHilang : longint;
+	tipeDataHilang : integer;
+	arrDataHilang : array [1..NMax] of DataHilang;
+
 	LoadNeffData : NeffData;
 
 	{ Deklarasi Fungsi/Prosedur }
@@ -69,6 +85,8 @@ interface
 		procedure LoadUser(getfileuser : string);
 		procedure LoadBuku(getfilebuku : string);
 		procedure LoadPeminjaman(getfilepeminjaman : string);
+		procedure LoadKembali(getfilekembali : string);
+		procedure LoadHilang(getfilehilang : string);
 	
 implementation
 procedure getFileName;
@@ -551,6 +569,248 @@ until (l >= lenBuku);
 Close(filebuku);
 
 end; // End of Algorithm to load Buku.csv
+
+procedure loadKembali(getfilekembali : string);
+begin
+// Algorithm to Read Pengembalian.csv
+
+// Mencari Banyak Characters pada file Pengembalian
+system.Assign(fkembali, getfilekembali);
+system.Reset(fkembali);
+lenKembali := FileSize(fkembali);
+Close(fkembali);
+// Akhir dari Mencari banyak characters
+
+LoadNeffData.Kembali := 0;
+i := 1; // Untuk Index array arrDataPeminjaman
+j := 1; // Untuk mendeteksi pembacaan char keberapa pada file filepengembalian
+tipeDataKembali := 1;
+newLineYearCounter := 0;
+dmyCounter := 1;
+
+system.Assign(filepengembalian, getfilekembali);
+system.Reset(filepengembalian);
+
+readln(filepengembalian,readline); // Skip line pertama
+
+read(filepengembalian, readchar);
+
+repeat 
+
+	case tipeDataKembali of
+	
+	1 : begin
+		if ( (readchar = ',') and (j < lenKembali) ) then
+		begin
+			read(filepengembalian, readchar);
+			j := j + 1;
+			tipeDataKembali := tipeDataKembali + 1;
+		end;
+		
+		while ( (readchar <> ',') and (j < lenKembali) and (tipeDataKembali = 1) ) do
+		begin
+		
+			arrDataKembali[i].username := arrDataKembali[i].username + readchar;
+			read(filepengembalian, readchar);
+			j := j + 1;
+		end;
+		
+		end;
+		
+	2 : begin
+		if ( (readchar = ',') and (j < lenKembali) ) then
+		begin
+			read(filepengembalian, readchar);
+			j := j + 1;
+			tipeDataKembali := tipeDataKembali + 1;
+		end;
+		
+		while ( (readchar <> ',') and (j < lenKembali) and (tipeDataKembali = 2) ) do
+		begin
+		
+			arrDataKembali[i].idBuku := arrDataKembali[i].idBuku + readchar;
+			read(filepengembalian, readchar);
+			j := j + 1;
+		end;
+		
+		end;
+		
+	3 : begin
+		
+		if ( (readchar = '/') ) then
+		begin
+			dmyCounter := dmyCounter + 1;
+			read(filepengembalian,readchar);
+			j := j + 1;
+		end;
+		
+		while ( (readchar <> ',') and (readchar <> '/') and (j < lenKembali) and (tipeDataKembali = 3) ) do
+		begin
+			
+			if (dmyCounter = 1) then
+			begin
+				arrDataKembali[i].tanggalKembali.day := arrDataKembali[i].tanggalKembali.day + readchar;
+				read(filepengembalian, readchar);
+				j := j + 1;
+			end else if (dmyCounter = 2) then
+			begin
+				arrDataKembali[i].tanggalKembali.month := arrDataKembali[i].tanggalKembali.month + readchar;
+				read(filepengembalian, readchar);
+				j := j + 1;
+			end else
+			begin
+				arrDataKembali[i].tanggalKembali.year := arrDataKembali[i].tanggalKembali.year + readchar;
+				newLineYearCounter := newLineYearCounter + 1;
+				if (newLineYearCounter = 4) then
+				begin
+					newLineYearCounter := 0;
+					tipeDataKembali := 1;
+					dmyCounter := 1;
+					i := i + 1;
+					LoadNeffData.Kembali := LoadNeffData.Kembali + 1;
+				end;
+				read(filepengembalian, readchar);
+				j := j + 1;
+			end;
+		end;
+		
+		if ( (readchar = ',') and (j < lenKembali) ) then
+		begin
+			readln(filepengembalian, readchar); // Next Line (Untuk Menghindari 'new line' dalam Array
+			read(filepengembalian, readchar);
+			j := j + 1;
+			tipeDataKembali := 1;
+		end;
+		
+		end;
+	
+	end;
+until (j >= lenKembali);
+		
+Close(filepengembalian);
+
+end; // End of Algorithm to Load pengembalian.csv
+
+procedure loadHilang(getfilehilang : string);
+begin
+// Algorithm to Read Pengembalian.csv
+
+// Mencari Banyak Characters pada file Pengembalian
+system.Assign(fHilang, getfilehilang);
+system.Reset(fHilang);
+lenHilang := FileSize(fHilang);
+Close(fHilang);
+// Akhir dari Mencari banyak characters
+
+LoadNeffData.Hilang := 0;
+i := 1; // Untuk Index array arrDataPeminjaman
+j := 1; // Untuk mendeteksi pembacaan char keberapa pada file filepengembalian
+tipeDataHilang := 1;
+newLineYearCounter := 0;
+dmyCounter := 1;
+
+system.Assign(filehilang, getfilehilang);
+system.Reset(filehilang);
+
+readln(filehilang,readline); // Skip line pertama
+
+read(filehilang, readchar);
+
+repeat 
+
+	case tipeDataHilang of
+	
+	1 : begin
+		if ( (readchar = ',') and (j < lenHilang) ) then
+		begin
+			read(filehilang, readchar);
+			j := j + 1;
+			tipeDataHilang := tipeDataHilang + 1;
+		end;
+		
+		while ( (readchar <> ',') and (j < lenHilang) and (tipeDataHilang = 1) ) do
+		begin
+		
+			arrDataHilang[i].username := arrDataHilang[i].username + readchar;
+			read(filehilang, readchar);
+			j := j + 1;
+		end;
+		
+		end;
+		
+	2 : begin
+		if ( (readchar = ',') and (j < lenHilang) ) then
+		begin
+			read(filehilang, readchar);
+			j := j + 1;
+			tipeDataHilang := tipeDataHilang + 1;
+		end;
+		
+		while ( (readchar <> ',') and (j < lenHilang) and (tipeDataHilang = 2) ) do
+		begin
+		
+			arrDataHilang[i].idBuku := arrDataHilang[i].idBuku + readchar;
+			read(filehilang, readchar);
+			j := j + 1;
+		end;
+		
+		end;
+		
+	3 : begin
+		
+		if ( (readchar = '/') ) then
+		begin
+			dmyCounter := dmyCounter + 1;
+			read(filehilang,readchar);
+			j := j + 1;
+		end;
+		
+		while ( (readchar <> ',') and (readchar <> '/') and (j < lenHilang) and (tipeDataHilang = 3) ) do
+		begin
+			
+			if (dmyCounter = 1) then
+			begin
+				arrDataHilang[i].tanggalLaporan.day := arrDataHilang[i].tanggalLaporan.day + readchar;
+				read(filehilang, readchar);
+				j := j + 1;
+			end else if (dmyCounter = 2) then
+			begin
+				arrDataHilang[i].tanggalLaporan.month := arrDataHilang[i].tanggalLaporan.month + readchar;
+				read(filehilang, readchar);
+				j := j + 1;
+			end else
+			begin
+				arrDataHilang[i].tanggalLaporan.year := arrDataHilang[i].tanggalLaporan.year + readchar;
+				newLineYearCounter := newLineYearCounter + 1;
+				if (newLineYearCounter = 4) then
+				begin
+					newLineYearCounter := 0;
+					tipeDataHilang := 1;
+					dmyCounter := 1;
+					i := i + 1;
+					LoadNeffData.Hilang := LoadNeffData.Hilang + 1;
+				end;
+				read(filehilang, readchar);
+				j := j + 1;
+			end;
+		end;
+		
+		if ( (readchar = ',') and (j < lenHilang) ) then
+		begin
+			readln(filehilang, readchar); // Next Line (Untuk Menghindari 'new line' dalam Array
+			read(filehilang, readchar);
+			j := j + 1;
+			tipeDataHilang := 1;
+		end;
+		
+		end;
+	
+	end;
+until (j >= lenHilang);
+		
+Close(filehilang);
+
+end; // End of Algorithm to Load Kehilangan.csv
 
 
 end.
