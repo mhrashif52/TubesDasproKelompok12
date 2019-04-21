d8618e499cf5f06667dcc07084d50725f08771b1
Herpeto <47052717+Herpeto@users.noreply.github.com>
2019-04-21 13:41:09 +0700

    Update Record_Perpus.pas

--- Record_Perpus.pas
+++ Record_Perpus.pas
@@ -6,10 +6,6 @@ interface
 		Akun = record
 				nama, alamat, username, password, role : string;
 				end;
-		Buku = record
-				id_buku, tahun_penerbit, jumlah_buku : integer;
-				judul_buku, pengarang, kategori : string;
-				end;
 		Tanggal = record
 				hari, bulan, tahun : string;
 				end;
