c09f1a238536766e00e251b072aa3e1d5555213f
Herpeto <47052717+Herpeto@users.noreply.github.com>
2019-04-21 15:51:20 +0700

    Update Statistik.pas

--- Statistik.pas
+++ Statistik.pas
@@ -1,23 +1,88 @@
 unit Statistik;
 {Unit yang digunakan untuk menunjukkan statistik yang ada pada perpustakaan}
+{BELUM SELESAI}
 
 interface
 	uses Record_Perpus;
-	procedure Statistik();
+	procedure Statistik(var arrDataBuku : array of DataBuku; arrDataAkun : array of akun);
 
 implementation
-	procedure Statistik();
+	procedure Statistik(var arrDataBuku : array of DataBuku; arrDataAkun : array of akun);
+		const
+			NMax = 10000;
+			
+		var
+			i : integer; {Variabel Untuk loop}
+			
+			admin,pengunjung,total_pengguna : integer; {Variabel penghitungan Pengguna}
+			sastra,sains,manga,sejarah,programming,total_buku : integer; {Variabel penghitungan Buku}
+			
 		begin
+			{Data Banyak Pengguna awal}
+			admin := 0;
+			pengunjung := 0;
+			total_pengguna := 0;
+			
+			{Data Banyak Buku awal}
+			sastra := 0;
+			sains := 0;
+			manga := 0;
+			sejarah := 0;
+			programming := 0;
+			total_buku := 0;
+			
+			for i:= 1 to (NMax-1) do
+				begin
+					{Penghitungan Data Akun}
+					if (arrDataAkun[i].role = 'admin') then
+						begin
+							admin := admin + 1;
+							total_pengguna := total_pengguna + 1;
+						end
+					else if(arrDataAkun[i].role = 'pengunjung') then
+						begin
+							pengunjung := pengunjung + 1;
+							total_pengguna := total_pengguna + 1;
+						end;
+					{Penghitungan Data Buku}	
+					if (arrDataBuku[i].kategori = 'sastra') then
+						begin
+							sastra := sastra + 1;
+							total_buku := total_buku + 1;
+						end
+					else if (arrDataBuku[i].kategori = 'sains') then
+						begin
+							total_buku := total_buku + 1;
+							sains := sains + 1;
+						end
+					else if (arrDataBuku[i].kategori = 'manga') then
+						begin
+							total_buku := total_buku + 1;
+							manga := manga + 1;
+						end
+					else if (arrDataBuku[i].kategori = 'sejarah') then
+						begin
+							total_buku := total_buku + 1;
+							sejarah := sejarah + 1;
+						end
+					else if (arrDataBuku[i].kategori = 'programming') then
+						begin
+							total_buku := total_buku + 1;
+							programming := programming + 1;
+						end;
+				end;
+			
+			{Penulisan Data}			
 			writeln('Pengguna:');
-			writeln('Admin | ');
-			writeln('Pengunjung | ');
-			writeln('Total | ');
+			writeln('Admin | ',admin);
+			writeln('Pengunjung | ',pengunjung);
+			writeln('Total | ',total_pengguna);
 			writeln('Buku:');
-			writeln('sastra | ');
-			writeln('sains | ');
-			writeln('manga | ');
-			writeln('sejarah | ');
-			writeln('programming | ');
-			writeln('Total | ');
+			writeln('sastra | ',sastra);
+			writeln('sains | ',sains);
+			writeln('manga | ',manga);
+			writeln('sejarah | ',sejarah);
+			writeln('programming | ',programming);
+			writeln('Total | ',total_buku);
 		end;
 end.
