59277a87e04a8f57d3fbbc31c385407e684fa818
Herpeto <47052717+Herpeto@users.noreply.github.com>
2019-04-21 14:43:55 +0700

    Update Login.pas

--- Login.pas
+++ Login.pas
@@ -3,11 +3,16 @@ unit Login;
 
 interface
 	uses Record_Perpus;
-	procedure Login(var Akun_Lama : akun);
+	procedure loginAkun(var arrDataAkun : array of akun);
 
 implementation
-	procedure Login(var Akun_Lama : akun);
+	procedure loginAkun(var arrDataAkun : array of akun);
+		const
+			NMax = 10000;
+		
 		var
+			i,valid : integer;
+			namaTemp : string;
 			Akun_Temp : akun;
 			
 		begin
@@ -15,15 +20,36 @@ implementation
 			readln(Akun_Temp.username);
 			write('Masukkan Password: ');
 			readln(Akun_Temp.password);
-			while (Akun_Lama.username <> Akun_Temp.username) or (Akun_Lama.password <> Akun_Temp.password) do
+			valid := 0;
+			
+			for i:= 1 to (NMax-1) do
+				begin
+					if(arrDataAkun[i].username = Akun_Temp.username) and (arrDataAkun[i].password = Akun_Temp.password) then
+						begin
+							valid := 1;
+							namaTemp := arrDataAkun[i].nama;
+						end;
+				end;
+
+			while (valid = 0) do
 				begin
 					writeln('Username / password salah! Silakan coba lagi.');
 					write('Masukkan Username: ');
 					readln(Akun_Temp.username);
 					write('Masukkan Password: ');
 					readln(Akun_Temp.password);
+						
+					for i:= 1 to (NMax-1) do
+						begin
+							if(arrDataAkun[i].username = Akun_Temp.username) and (arrDataAkun[i].password = Akun_Temp.password) then
+								begin
+									valid := 1;
+									namaTemp := arrDataAkun[i].nama;
+								end;
+						end;
 				end;
-			writeln('Selamat Datang ',Akun_Lama.nama,'!');
+			
+			writeln('Selamat Datang ',namaTemp,'!');
 		end;
 end.	
 		
