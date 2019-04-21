0feedd243b33440963be515f1161508a5909cc00
Rainburn <49584704+Rainburn@users.noreply.github.com>
2019-04-21 13:40:01 +0700

    Update Record_Perpus.pas

--- Record_Perpus.pas
+++ Record_Perpus.pas
@@ -14,6 +14,28 @@ interface
 				hari, bulan, tahun : string;
 				end;
 
+		Date = record
+			day : string;
+			month : string;
+			year : string;
+			end;
+
+		DataPeminjaman = record
+			idBuku : string;
+			username : string;
+			judul : string;
+			datePinjam : Date;
+			batasKembali : Date;
+			end;
+
+		DataBuku = record
+			idBuku : string;
+			judul : string;
+			author : string;
+			jumlah : string;
+			tahunterbit : string;
+			kategori : string;
+			end;
 implementation
 end.
 				
