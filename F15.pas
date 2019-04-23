0d71d87031b3eb428b0debe391f7755d9d6407e5
edoariasena <49637330+edoariasena@users.noreply.github.com>
2019-04-23 21:33:29 +0700

    Update F15.pas

--- F15.pas
+++ F15.pas
@@ -1,26 +1,28 @@
-program cari_anggota;
+Unit F15;
 {Mencari data diri anggota perpustakaan dilakukan validasi terlebih 
 * apakah username terdaftar ke dalam database perpustakaan }
 
-{ KAMUS }
-type
+interface
+uses F13, record_perpus;
+{type
 	DataUser = Record
 		Username : String;
 		Nama : String;
 		Alamat : String;
-	end;
+	end;}
 var
-	CariUser : file of DataUser;
-	data_user : DataUser;
+	//data_user : Akun;
 	InUsername : String;
-	TabDataUser : Array [1..1000000] of DataUser;
+	//TabDataUser : Array [1..1000000] of Akun;
 	i : Integer;
+	
+	procedure CariDataDiri (var InUsername : string);
 
 { ALGORITMA }
+implementation
+procedure CariDataDiri (var InUsername : string);
 begin
-	assign(CariUser, 'user.csv');
-	reset(CariUser);
-	i := 1;
+	{i := 1;
 	repeat
 		read(CariUser, data_user);
 		TabDataUser[i].Username := data_user.Username;
@@ -29,14 +31,17 @@ begin
 		i := i + 1;
 	until (eof(CariUser));
 	Write('Masukkan Username: ');
-	readln(InUsername);
-	if (InUsername = TabDataUser[i].Username) then
-	begin
-		writeln('Nama Anggota: ', TabDataUser[i].Nama);
-		writeln('Alamat Anggota: ', TabDataUser[i].Alamat);
-	end
-	else if (InUsername <> TabDataUser[i].Username) then
-	begin
-		writeln('Pengguna tidak ditemukan');
-	end
+	readln(InUsername);}
+	for i := 1 to 10000 do;
+		begin
+			if (InUsername = arrDataAkun[i].Username) then
+				begin
+					writeln('Nama Anggota: ', arrDataAkun[i].Nama);
+					writeln('Alamat Anggota: ', arrDataAkun[i].Alamat);
+			end else if (InUsername <> arrDataAkun[i].Username) then
+				begin
+					writeln('Pengguna tidak ditemukan');
+				end;
+		end;
+end;
 end.
