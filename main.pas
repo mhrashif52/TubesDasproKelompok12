e85a25ba034ed328082963d11cb57b56bf119ab1
edoariasena <49637330+edoariasena@users.noreply.github.com>
2019-04-23 17:25:26 +0700

    Update main.pas

--- main.pas
+++ main.pas
@@ -106,18 +106,16 @@ begin
 	begin
 		writeln('1  - Pencarian Buku berdasarkan Kategori');
 		writeln('2  - Pencarian Buku berdasarkan tahun');
-		writeln('3  - Peminjaman Buku');
-		writeln('4  - Pengembalian Buku');
-		writeln('5  - Melaporkan Buku Hilang');
-		writeln('6  - Melihat laporan buku yang hilang');
-		writeln('7  - Menambahkan Buku');
-		writeln('8  - Melakukan Penambahan jumlah buku ke sistem');
-		writeln('9  - Melihat Riwayat Peminjaman');
-		writeln('10 - Statistik');
-		writeln('11 - Pencarian Anggota');
-		writeln('12 - Register Akun');
-		writeln('13 - Save');
-		writeln('14 - Exit Program');
+		writeln('3  - Melaporkan Buku Hilang');
+		writeln('4  - Melihat laporan buku yang hilang');
+		writeln('5  - Menambahkan Buku');
+		writeln('6  - Melakukan Penambahan jumlah buku ke sistem');
+		writeln('7  - Melihat Riwayat Peminjaman');
+		writeln('8 - Statistik');
+		writeln('9 - Pencarian Anggota');
+		writeln('10 - Register Akun');
+		writeln('11 - Save');
+		writeln('12 - Exit Program');
 		writeln('');
 		write('Masukkan pilihan: ');
 		readln(doAction);
@@ -132,22 +130,13 @@ begin
 		2 : begin
 				//procedure 04
 			end;
-
+			
 		3 : begin
-				//procedure 05
-			end;
-
-		4 : begin
-				write('Masukkan id buku yang dikembalikan: ');
-				readln(idBuku);
-				Pengembalian(usernameLogin,idBuku);
-			end;
-		5 : begin
 				//procedure 07
 				laporHilang(arrDataHilang);
 			end;
 		
-		6 : begin
+		4 : begin
 				//procedure 08
 				//LihatLaporanHilang
 				writeln('Ketik "Lihat buku hilang" untuk melihat daftar buku yang hilang');
@@ -155,34 +144,34 @@ begin
 				LihatLap(com);
 			end;
 		
-		7 : begin
+		5 : begin
 				//procedure 09
 			end;
 
-		8 : begin
+		6 : begin
 				//procedure 10
 			end;
 
-		9 : begin
+		7 : begin
 				write('Masukkan username pengunjung: ');
 				readln(UsernameRiwayat);
 				RiwayatPengembalian(UsernameRiwayat);
 			end;
 		
-		10 : begin
+		8 : begin
 				//procedure 12
 				Statistik(arrDataBuku,arrDataAkun);
 			end;
 
-		11 : begin
+		9 : begin
 				//procedure 13
 			end;
 
-		12 : begin
+		10 : begin
 				registerAkun;
 			end;
 		
-		13 : begin
+		11 : begin
 				//Procedure 14
 				getFileNameSave;
 				SaveDataPeminjaman(NamaFilePeminjaman);
@@ -192,7 +181,7 @@ begin
 				SaveDataKehilangan(NamaFileKehilangan);
 			end;
 
-		14 : begin
+		12 : begin
 				write('Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ');
 				readln(SaveOrNot);
 				if (SaveOrNot = 'Y') or (SaveOrNot = 'y') then
